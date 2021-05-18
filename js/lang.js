4d5f04b0c59d2f2892fdbfa4680a69edf5f236be
giuseppe naponiello <beppenapo@arc-team.com>
2021-05-18 17:21:23 +0200

    recoding app

--- js/lang.js
+++ js/lang.js
@@ -1,10 +1,47 @@
 const gui = {
-  "TourSelect":{
+  "tourSelect":{
     "ita":"Scegli il tuo Tour!",
     "eng":"Choose your Tour!"
   },
   "updateAvailable":{
     "ita":"<p class='mb-2 font-weight-bold'>Nuova versione disponibile!</p><button type='button' class='btn btn-primary btn-sm mx-auto' id='reload'>Ok, aggiorna</button>",
     "eng":"<p class='mb-2 font-weight-bold'>New version available!</p><button type='button' class='btn btn-primary btn-sm mx-auto' id='reload'>Ok, update</button>"
+  },
+  "tour":{
+    "monteArgentario":{
+      "logo":"MA_icon.png",
+      "titolo":"Monte Argentario",
+      "itaTxt":"Breve descrizione in italiano. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "engTxt":"Short english description. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "link":"https://arcteam.github.io/virtualArch.github.io/"
+    },
+    "giardinoCiucioi":{
+      "logo":"GC_icon.png",
+      "titolo":"Giardino dei Ciucioi",
+      "itaTxt":"Breve descrizione in italiano. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "engTxt":"Short english description. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "link":""
+    },
+    "claudiaAugusta":{
+      "logo":"VCA_icon.png",
+      "titolo":"via Claudia Augusta",
+      "itaTxt":"Breve descrizione in italiano. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "engTxt":"Short english description. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "link":""
+    },
+    "orrido":{
+      "logo":"PA_icon.png",
+      "titolo":"Orrido di Ponte Alto",
+      "itaTxt":"Breve descrizione in italiano. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "engTxt":"Short english description. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "link":""
+    },
+    "castagneto":{
+      "logo":"CS_icon.png",
+      "titolo":"Castagneto storico",
+      "itaTxt":"Breve descrizione in italiano. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "engTxt":"Short english description. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "link":""
+    },
   }
 }
